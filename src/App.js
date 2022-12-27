3dd575cf9e059cdd31e46030a6419bb088f917f2
EniolaAdemola <it.eniolaademola@gmail.com>
2022-12-27 09:37:59 -0800

    handle all route pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import {
+	Home,
+	Cards,
+	Spemd,
+	Save,
+	Budget,
+	Borrow,
+	Business,
+	Press,
+	Workable,
+	About,
+	Help,
+	FAQs,
+	Security,
+	ContactUs,
+	Login,
+	GetKuda,
+} from "./pages";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<div>
+			<div>
+				<BrowserRouter>
+					<Routes>
+						<Route index element={<Home />} />
+
+						{/* Features SubPages */}
+						<Route path="cards" element={<Cards />} />
+						<Route path="spend" element={<Spemd />} />
+						<Route path="save" element={<Save />} />
+						<Route path="budget" element={<Budget />} />
+						<Route path="borrow" element={<Borrow />} />
+
+						{/* Business Page */}
+						<Route path="business" element={<Business />} />
+						<Route path="press" element={<Press />} />
+						<Route path="workable" element={<Workable />} />
+						<Route path="about" element={<About />} />
+
+						{/* Help SubPages */}
+						<Route path="help" element={<Help />} />
+						<Route path="faqs" element={<FAQs />} />
+						<Route path="security" element={<Security />} />
+						<Route path="contact" element={<ContactUs />} />
+
+						{/* Register / Login */}
+						<Route path="login" element={<Login />} />
+						<Route path="getKuda" element={<GetKuda />} />
+
+						{/* NotFound page redirect to home page */}
+						<Route path="*" element={<Navigate to="/" />} />
+					</Routes>
+				</BrowserRouter>
+			</div>
+		</div>
+	);
+};
 
 export default App;
